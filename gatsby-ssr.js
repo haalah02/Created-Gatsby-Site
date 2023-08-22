998b00bc8fafd47dce38049e1321812b83351d9d
haalahibrahim03 <mdl19ebe025@mec.ac.in>
2023-08-22 14:55:40 +0530

    code changes

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,7 +3,7 @@ import React from "react";
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <script
-      key="cookieyes-script"
+      // key="cookieyes-script"
       id="cookieyes"
       type="text/javascript"
       src="https://scriptstaging.cookieyes.com/client_data/6722d32f6a3aad938b223092/script.js"
