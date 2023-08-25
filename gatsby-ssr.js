cbd67d72aca2a9d0b6e43bc6165938e8d1125b77
haalah02 <140680984+haalah02@users.noreply.github.com>
2023-08-25 12:01:36 +0530

    new banner gatsby-ssr.js
    Have to add privacy policy, cookie policy and affiliate link on gatsby site. So added the site with new installation code in haalah+45 free live app. Haven't removed site from QA yet.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,7 +6,7 @@ export const onRenderBody = ({ setHeadComponents }) => {
       // key="cookieyes-script"
       id="cookieyes"
       type="text/javascript"
-      src="https://scriptstaging.cookieyes.com/client_data/6722d32f6a3aad938b223092/script.js"
+      src="https://cdn-cookieyes.com/client_data/494f1850bb481d55a2d7384a/script.js"
     />,
   ]);
 };
