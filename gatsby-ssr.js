80e97ac05058a5f3264b242b2de5fcc2723ac3c2
haalah02 <140680984+haalah02@users.noreply.github.com>
2023-08-25 12:47:12 +0530

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,7 +8,7 @@ export const replaceComponentRenderer = ({ props }) => {
     <footer>
       {layout.footer}
       <div>
-        <Link to="src/pages/cookie-policy.md">Cookie Policy</Link>
+        <Link to="/cookie-policy">Cookie Policy</Link>
       </div>
     </footer>
   );
