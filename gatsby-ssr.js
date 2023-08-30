701f16b7a90716dec1597def443da572d53bf967
haalahibrahim03 <mdl19ebe025@mec.ac.in>
2023-08-30 15:09:46 +0530

    banner code removed
    the banner code is commented in gatsby-ssr.js so that the created gatsby site can be edited out of haalah+45 live app and vue site can be added to it instead.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -20,13 +20,13 @@ import React from "react";
 // };
 
 
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script
-      // key="cookieyes-script"
-      id="cookieyes"
-      type="text/javascript"
-      src="https://cdn-cookieyes.com/client_data/494f1850bb481d55a2d7384a/script.js"
-    />,
-  ]);
-};
+// export const onRenderBody = ({ setHeadComponents }) => {
+//   setHeadComponents([
+//     <script
+//       // key="cookieyes-script"
+//       id="cookieyes"
+//       type="text/javascript"
+//       src="https://cdn-cookieyes.com/client_data/494f1850bb481d55a2d7384a/script.js"
+//     />,
+//   ]);
+// };
